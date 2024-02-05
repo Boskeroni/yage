a9078fc41a86449ceb88cb3f85d55bac4fe3d6f9
Leo <boskeroni@gmail.com>
2024-02-05 19:54:04 +0000

    added args.rs file

---
+++ src/args.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, allow_hyphen_values(true))]
+pub struct Args {
+    pub rom_name: String,
+
+    #[arg(short, long)]
+    pub booted: bool,
+
+    
+}
